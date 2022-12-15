a4f0224ac26d2c79980deb146a31f0c33b3e18cc
Dmitry Pundyk <Nekucfyekesh.f@gmail.com>
2022-12-15 20:04:03 +0300

    Подключен Spring boot

--- src/main/java/by/clevertec/receipt/ReceiptApplication.java
+++ src/main/java/by/clevertec/receipt/ReceiptApplication.java
@@ -1,7 +1,17 @@
 package by.clevertec.receipt;
 
-public class ReceiptApplication {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptApplication implements CommandLineRunner {
     public static void main(String[] args) {
+        SpringApplication.run(ReceiptApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args){
         System.out.println("Gradle project");
     }
 }
---
+++ src/test/java/by/clevertec/receipt/ReceiptApplicationTest.java
@@ -0,0 +1,13 @@
+package by.clevertec.receipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ReceiptApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
